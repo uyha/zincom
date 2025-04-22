16005959a288831cc052da296b7651f26f2f113e
Uy Ha <hchanuy@gmail.com>
2025-04-22 15:21:50 +0000

    use proper static variable

--- src/utils.zig
+++ src/utils.zig
@@ -1,19 +1,40 @@
-threadlocal var dummy_message: ?zimq.Message = null;
-
 /// Get all message parts from the socket
 pub fn consumeAll(socket: *zimq.Socket) zimq.Socket.RecvError!void {
-    if (dummy_message == null) {
-        dummy_message = .empty();
+    const static = struct {
+        threadlocal var buffer: ?zimq.Message = null;
+    };
+    if (static.buffer == null) {
+        static.buffer = .empty();
     }
+
     while (true) {
         _ = socket.recvMsg(
-            &dummy_message.?,
+            &static.buffer.?,
             .{},
         ) catch |err| return @errorCast(err);
-        if (!dummy_message.?.more()) {
+        if (!static.buffer.?.more()) {
             break;
         }
     }
+}
+
+test consumeAll {
+    var context: *zimq.Context = try .init();
+    defer context.deinit();
+
+    var push: *zimq.Socket = try .init(context, .push);
+    defer push.deinit();
+
+    var pull: *zimq.Socket = try .init(context, .pull);
+    defer pull.deinit();
+
+    try push.bind("inproc://#1");
+    try pull.connect("inproc://#1");
+
+    try push.sendConstSlice("", .more);
+    try push.sendConstSlice("", .{});
+
+    try consumeAll(pull);
 }
 
 pub fn StructAsTaggedUnion(Data: type) type {
