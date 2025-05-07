a6f2c0ae77e55c08ca81580ed73f23e0ef1d8aab
Uy Ha <hchanuy@gmail.com>
2025-05-07 09:57:09 +0000

    add test for `checkMemebers`

--- tests/head_nerve.zig
+++ tests/head_nerve.zig
@@ -35,43 +35,6 @@ test "Head and Nerve join" {
     }
 }
 
-test "Head and Nerve ping" {
-    const allocator = t.allocator;
-
-    var context: *zimq.Context = try .init();
-    defer context.deinit();
-
-    var head: zic.Head = try .init(context, "inproc://#1/head");
-    defer head.deinit(allocator);
-
-    var nerve: zic.Nerve = try .init(context, "inproc://#1/head");
-    defer nerve.deinit(allocator);
-
-    try nerve.sendJoin(allocator, "test", 200, .initComptime(
-        .{
-            &.{ "hello", "inproc://#1/hello" },
-        },
-    ));
-    try head.process(allocator);
-    _ = try nerve.getResponse(allocator);
-
-    {
-        try nerve.sendPing(allocator, "test");
-        try head.process(allocator);
-
-        const response = try nerve.getResponse(allocator);
-        try t.expectEqual(zic.Resp{ .ping = .success }, response);
-    }
-
-    {
-        try nerve.sendPing(allocator, "nopenoneverexists");
-        try head.process(allocator);
-
-        const response = try nerve.getResponse(allocator);
-        try t.expectEqual(zic.Resp{ .ping = .absence }, response);
-    }
-}
-
 test "Head and Nerve down" {
     const allocator = t.allocator;
 
@@ -125,8 +88,53 @@ test "Head and Nerve down" {
     }
 }
 
+test "Head and Nerve checkMembers" {
+    const allocator = t.allocator;
+
+    var context: *zimq.Context = try .init();
+    defer context.deinit();
+
+    var head: zic.Head = try .init(context, "inproc://#1/head");
+    defer head.deinit(allocator);
+
+    var nerve: zic.Nerve = try .init(context, "inproc://#1/head");
+    defer nerve.deinit(allocator);
+
+    try nerve.sendJoin(allocator, "test", 10 * ns_per_ms, .initComptime(
+        .{
+            &.{ "hello", "inproc://#1/hello" },
+        },
+    ));
+    try head.process(allocator);
+    _ = try nerve.getResponse(allocator);
+
+    {
+        sleep(5 * ns_per_ms);
+        try head.checkMembers(allocator);
+
+        try nerve.sendPing(allocator, "test");
+        try head.process(allocator);
+
+        const response = try nerve.getResponse(allocator);
+        try t.expectEqual(zic.Resp{ .ping = .success }, response);
+    }
+
+    {
+        sleep(11 * ns_per_ms);
+        try head.checkMembers(allocator);
+
+        try nerve.sendPing(allocator, "test");
+        try head.process(allocator);
+
+        const response = try nerve.getResponse(allocator);
+        try t.expectEqual(zic.Resp{ .ping = .absence }, response);
+    }
+}
+
 const std = @import("std");
 const t = std.testing;
+const ns_per_ms = std.time.ns_per_ms;
+const sleep = std.Thread.sleep;
 
 const zic = @import("zic");
 const zimq = @import("zimq");
